870101a2f6af2990169970bb882cb1aa6d65ffe4
Miguel Angel Rincon <miguel.angel.rincon@estudiantat.upc.edu>
2021-05-04 16:56:59 +0200

    auth arreglado
    He quitado el campo required de role en el model user

--- src/models/user.ts
+++ src/models/user.ts
@@ -47,7 +47,8 @@ const userSchema = new Schema({
     },
     role: {
         type: Number, // 0 user, 1 admin, 2 coach
-        required: true
+        required: false,
+        default: 0
     },
     photos: [
         {
