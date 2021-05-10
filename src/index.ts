f1749fff1372ed7f2af3cc4afaa1b392a493726a
Kevin Alcalde <kevin.javier.alcalde@estudiantat.upc.edu>
2021-05-10 10:14:44 +0200

    revision Toni

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,35 @@
 import dotenv from 'dotenv';
-import SocketIO from 'socket.io'
-
 dotenv.config();
 import { startConnection } from './database'
-
 import app from './app';
 import { idText, isConstructorDeclaration } from 'typescript';
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
-import user from 'models/user'
-
-
 var express = require('express');
 const server = createServer(app);
-//var server = require('http').Server(app);
-//const https = require("https"),
-//  fs = require("fs");
-//const server = https.createServer(Option, app).listen(8100);
-
-//var socket = require('socket.io');
+import user, {IUser} from './models/user'
 const io = new Server(server, { cors: { origin: '*' } });
 //const io = new Server(server);
+let userX: IUser;
+let usuario: IUser; //
+let ListaUser: Map<string, IUser> = new Map();
+let ListaConectado: Array<IUser> = new Array();
+var i= [];
 
-
-
-var messages = [{
-    id:1,
-    text: "Nombre",
-    author: "Texto de prueba"
-}];
-
-
-
+var messages = [{}];
 io.on('connection', (socket:Socket) => {
+    let username: string;
+    let id: number;
     console.log("nueva conexion");
-    socket.on('new-message', function(data) {
-<<<<<<< HEAD
-        console.log('el objeto que me lo entrega es: ',data);
-        
-        //io.sockets.emit('messages', messages);
-
-          
-    });
-    socket.on('disconnect', function () {
-        console.log('Se ha desconectado alguien');
-=======
+    socket.on('me-conecto', function(data) {
         userX = data;
         username= userX.username;
+       
         id= userX._id;
-        ListaUser.set(username, id)
-        console.log("El usuario es ", userX);
-        io.emit('listausuarios', Array.from(ListaUser));       
+        ListaUser.set(username, userX)
+        
+        console.log("El usuario es ", username);
+        io.emit('listausuarios', Array.from(ListaUser));
     });
     socket.on('disconnect', function () {
         if(username){
@@ -59,10 +38,10 @@ io.on('connection', (socket:Socket) => {
             io.emit('listausuarios', Array.from(ListaUser))
 
         }
->>>>>>> d0f8e0318366a9f7f91cec334de5d4edf56b8886
     });
 });
 function init() {
+    let usuarios: IUser[] = [];
     startConnection();
     //app.listen(app.get('port'));
     //console.log('Server on port', 3000);
