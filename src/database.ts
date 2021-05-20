99319338a67be0254648fccb06146b331c91ec2c
carlosgm15 <carlos98gm@gmail.com>
2021-05-20 23:59:06 +0200

    dockerfileback

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export async function startConnection() {
-    await mongoose.connect('mongodb://localhost:27017/gymder',{
+    await mongoose.connect('mongodb://mongo:27017/gymder',{
         useNewUrlParser: true,
         useFindAndModify: false ,
         useUnifiedTopology: true,
