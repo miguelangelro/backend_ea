450f26267f3ee96ea3f00cbb06d3483602398743
jordi0907 <jrodrisant@hotmail.com>
2021-05-21 20:07:50 +0200

    login con redes sociales y recuperacion de contraseña

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 const router = Router();
 
-import { signup, signin, profile, getMe } from '../controllers/auth.controller'
+import { signup, signin, profile, getMe, forgotPassword, checkemail, signinRedesSociales} from '../controllers/auth.controller'
 import { TokenValidation } from '../middlewares/verifyToken'
 
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.get('/profile', TokenValidation, profile)
 router.get('/me',TokenValidation, getMe);
+router.post('/forgotpassword', forgotPassword);
+router.post('/checkemail', checkemail);
+router.post('/signinrs', signinRedesSociales)
+
 
 export default router;
